de38976fb7d8a754bfab16cf765e4eaa66b19e23
roeyashernielsen <roey.asher@nielsen.com>
2016-05-24 01:19:26 +0300

    Get start working with Django... Creating basic html Creating css file

--- Markit/models.py
+++ Markit/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class System(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+    description = models.TextField
