ac0d0514c8029f137b8e7eaa7b449af8139c2d91
roeyashernielsen <roey.asher@nielsen.com>
2016-06-06 23:17:26 +0300

    Adding django models for the database, initial

--- Markit/models.py
+++ Markit/models.py
@@ -3,4 +3,9 @@ from django.db import models
 
 class System(models.Model):
     name = models.CharField(max_length=256, null=False, blank=False)
-    description = models.TextField
+    description = models.TextField(null=True, blank=False)
+
+class Stock(models.Model):
+    name = models.CharField(max_length=256, null=False, blank=False)
+    symbol = models.CharField(max_length=256, null=False, blank=False)
+    file = models.FileField(default="tmp",upload_to='files/%Y/%m/%d')
