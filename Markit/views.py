bad87857c7399488034ed77de79ce9dc56f4f0ef
roeyashernielsen <roey.asher@nielsen.com>
2016-06-18 19:18:49 +0300

    insert myform with the ability to duplicate fields

--- Markit/views.py
+++ Markit/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, render_to_response
 from .models import Stock
+from django import forms
 
 
 def index(request):
@@ -17,3 +18,28 @@ def index(request):
 def form(request):
     return render(request, 'Markit/form.html', {
     })
+
+
+class MyForm(forms.Form):
+    original_field = forms.CharField()
+    extra_field_count = forms.CharField(widget=forms.HiddenInput())
+
+    def __init__(self, *args, **kwargs):
+        extra_fields = kwargs.pop('extra', 0)
+
+        super(MyForm, self).__init__(*args, **kwargs)
+        self.fields['extra_field_count'].initial = extra_fields
+
+        for index in range(int(extra_fields)):
+            # generate extra fields in the number specified via extra_fields
+            self.fields['extra_field_{index}'.format(index=index)] = forms.CharField()
+
+
+def myform(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST, extra=request.POST.get('extra_field_count'))
+        if form.is_valid:
+            print("valid!")
+    else:
+        form = MyForm()
+    return render(request, 'Markit/myform.html', { 'form': form })
