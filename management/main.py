f79ee7f93cd2b61111a7e3c8ae4747f781b9202b
roeyashernielsen <roey.asher@nielsen.com>
2016-06-24 15:30:36 +0300

    init push

--- management/main.py
+++ management/main.py
@@ -8,7 +8,6 @@ from utils import enums
 
 
 def main(argv=None):
-
 	path = "/Users/roeya/Desktop/stock/"
 	stocks = {}
 
@@ -16,18 +15,33 @@ def main(argv=None):
 		if file.endswith(".csv"):
 			stocks[file.rsplit('.', 1)[0]] = pd.read_csv(path + file)
 
-	# print(enums.SUPPORTED_INDICATORS.ema)
-	# tp1 = TechnicalParameter()
-	# open_rule = Rule()
-	# close_rule = Rule()
-	# sys = TradeSystem()
+	print(enums.SUPPORTED_INDICATORS.ema)
 
-# def parse_indicators(trade_system):
-# 	indicators = []
-# 	for trem in trade_system.get_open_rule().get_clauses().get_terms():
-# 		indicators.append(term.get_technical_parameter_1())
-# 	close_rule = trade_system.get_close_rule()
-#
+
+# tp1 = TechnicalParameter()
+# open_rule = Rule()
+# close_rule = Rule()
+# sys = TradeSystem()
+
+def parse_indicators(trade_system):
+	indicators = []
+	terms = trade_system.get_open_rule().get_clauses().get_terms() + trade_system.get_close_rule().get_clauses().get_terms()
+	for term in terms:
+		tp1 = term.get_technical_parameter_1
+		tp2 = term.get_technical_parameter_1
+		if(tp1.is_technical_indicator()):
+			indicators.append(term.get_technical_parameter_1)
+		if (tp2.is_technical_indicator()):
+			indicators.append(term.get_technical_parameter_1)
+	return indicators
+
+def indicator_equality(indicator1, indicator2):
+	if(indicator1.get_name == indicator2.get_name):
+		if(indicator1.get_period == indicator2.get_period):
+			return True
+	return False
+
+def
 
 
 if __name__ == "__main__":
