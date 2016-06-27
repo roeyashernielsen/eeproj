688b1a86f62c1f9ffd2ec2ba0b11a148c8390dc5
Tzion Sasson <tzion12345@gmail.com>
2016-06-27 21:56:01 +0300

    trade statsitsics: finish classes and fields, docu was added

--- statistics/system_statistics.py
+++ statistics/system_statistics.py
@@ -1,20 +1,56 @@
 from .trades_statistics import calculate_trades_statistics
 from numpy import std, average
 
+
 """
 This file liable for the statistics calculation of the trading system.
 It uses the statistic calculations of each trade in trading period of stock to retrieve the stock statistics, and
 based on those, it derives the statistics of the whole trading system.
+"""
 
-LIST OF ALL STATISTICS:
-System Statistics:
-all the stock statistics plus:
-    + start date, close date
-    + period (days)
-    + average open trades at single point over time
-    + max open trades on single point of time
 
-Stock Statistics:
+def calculate_system_statistics(stock_data_table_dict, direction, period_days, start_date, end_date, system_name):
+    """
+    This function does the calculation of the systems and the stocks statistics.
+    :param stock_data_table_dict: dictionary where the key is the stock symbol, and the value is tuple of:
+    (trades_table, start_date, end_date, period_days)- all are match to the specific stock.
+    trades_table is a DataFrame table contains all the trades on that stock,
+    start_date and end_date are the first and the last day of original stock table (before the filtering),
+    period is the total amount of trading days between the start and the end.
+    So the format is: {SYMBOL1=(trades_table1, start_date1, end_date1, period_days1), SYMBOL2=...}
+    :param direction: the direction of the trade - long/short (define in the right enum)
+    :param system_name: the name of system as the user defined
+    :return: tuple of: (SystemStatistics, [StockStatistics]), the second item is list contains all stock statistics.
+    """
+
+    # retrieve all the trade of all the stocks in the trade system
+    system_trades = {}  # dictionary with stock symbols as keys and list of all trades of that stock as value
+    for stock in stock_data_table_dict:
+        system_trades.update({stock: calculate_trades_statistics(stock_data_table_dict[stock], direction)})
+
+    stocks_statistics_list = []
+    system_statistics = SystemStatistics(system_name, period_days, [], [], [], start_date, end_date)
+    # iterate over all stocks create StockStatistics objects
+    for stock in system_trades:
+        stock_statistics = StockStatistics(stock, period_days, [trade.get_duration() for trade in system_trades[stock]],
+                                           [trade.get_profit_points() for trade in system_trades[stock]],
+                                           [trade.get_profit_percentage() for trade in system_trades[stock]])
+        stock_statistics.calculate_statistics()  # calculate each stock's statistics
+        stocks_statistics_list.extend([stock_statistics])  # add it to the output list
+
+        # extend system stats vectors with the stock's vector
+        system_statistics.durations_vector += stock_statistics.durations_vector
+        system_statistics.yields_points_vector += stock_statistics.yields_points_vector
+        system_statistics.yields_percentages_vector += stock_statistics.yields_percentages_vector
+    system_statistics.calculate_statistics()  # finally calculate the system stats, based on the concatenated vectors of all stocks
+
+    return system_statistics, stocks_statistics_list
+
+
+class StockStatistics:
+    """
+    Class to store the statistics of stock, and does the calculation of them.
+    Some of the class statistics:
     + total number of trades
     + average gain ($/%) + stdev
     + profit/loss ratio ($:$)
@@ -24,73 +60,29 @@ Stock Statistics:
     + max losing trade ($/%)
     + average winning trade ($/%) + stdev
     + average losing trade ($/%) + stdev
-    + period (days)
     + % time of open position
     + average days of holding per trade
 
-
-"""
-
-"""
-TODOs:
-    1. finish SystemStatistics class, additional fields and override the calculation method
-    2. write function that convert statistics to dataframe (or other represented frame)
-    3. add documentation
-    
-"""
-
-def calculate_system_statistics(stock_trade_table_dict, direction, period_days, start_date, end_date, system_name):
     """
-    This function does the calculation of the systems and the stocks statistics.
-    :param stock_trade_table_dict: dictionary where the key is the stock symbol, and the value is DataFrame object
-    table that contains all the trades on that stock
-    :param direction: the direction of the trade - long/short (define in the right enum)
-    :return:
-    """
-
-    # retrieve all the trade of all the stocks in the trade system
-    system_trades = {}  # dictionary with stock symbols as keys and list of all trades of that stock as value
-    for stock in stock_trade_table_dict:
-        system_trades.update({stock: calculate_trades_statistics(stock_trade_table_dict[stock], direction)})
-
-    stocks_statistics_list = []
-    system_statistics = SystemStatistics(system_name, period_days, [], [], [], start_date, end_date)
-    for stock in system_trades:
-        stock_statistics = StockStatistics(stock, period_days, [trade.get_duration() for trade in system_trades[stock]],
-                                           [trade.get_profit_points() for trade in system_trades[stock]],
-                                           [trade.get_profit_percentage() for trade in system_trades[stock]])
-        stock_statistics.calculate_statistics()
-        stocks_statistics_list.extend([stock_statistics])
-
-        # extend system stats vectors with the stock's vector
-        system_statistics.durations_vector += stock_statistics.durations_vector
-        system_statistics.yields_points_vector += stock_statistics.yields_points_vector
-        system_statistics.yields_percentages_vector += stock_statistics.yields_percentages_vector
-    system_statistics.calculate_statistics()
-
-    return system_name, stocks_statistics_list
-
-
-class StockStatistics:
-    """
-    Class to store the statistics of stock
-    """
-    def __init__(self, symbol, period, durations_vector, yields_points_vector, yields_percentages_vector):
+    def __init__(self, symbol, start_date, end_date, period, durations_vector, yields_points_vector, yields_percentages_vector):
 
         self.name = symbol  # the symbol of the stock
+        self.start_date = start_date
+        self.end_date = end_date
         self.period = period
         # All statistics calculations are based on the next 3 vectors:
         self.durations_vector = durations_vector
         self.yields_points_vector = yields_points_vector
         self.yields_percentages_vector = yields_percentages_vector
         
-        # Derived vectors (order 0 stats)
+        # Derived vectors (order 0 statistics)
         self.profit_points_vector = [y for y in yields_points_vector if y > 0]
         self.profit_percentages_vector = [y for y in yields_percentages_vector if y > 0]
         self.loss_points_vector = [y for y in yields_points_vector if y < 0]
         self.loss_percentages_vector = [y for y in yields_percentages_vector if y < 0]
 
-        # Accumulating fields (order 1 stats)
+        # All following fields are calculated later on, by calling to calculate_statistics()
+        # Accumulating fields (order 1 statistics)
         self.trades = 0  # total amount of trades
         self.total_holding_period = 0
         self.yield_points = 0.0  # in point ($)
@@ -98,7 +90,7 @@ class StockStatistics:
         self.winning_trades = 0  # number of winning trades
         self.losing_trades = 0  # number of losing trades
 
-        # Calculative fields (order 2 stats)
+        # Calculative fields (order 2 statistics)
         self.profit_loss_ratio_points = 0.0  # profit/loss ($/$)
         self.profit_loss_ratio_percentages = 0.0  # profit/loss (%/%)
         self.max_profit_points = 0.0  # the most profitable trade ($)
@@ -111,11 +103,13 @@ class StockStatistics:
         self.stdev_yield_percentages = 0.0
         self.average_holding_period = 0.0
         self.stdev_holding_period = 0.0
+        self.efficiency = 0.0  # % of time of open position
 
     def calculate_statistics(self):
         """
-        TODO
-        :return:
+        This method calculates the stock statistics, that are based on the data vectors.
+        It should be called once, only after all the data were collected into those vectors (basically this should be
+        done on the class creation).
         """
         # first make the vector immutable
         self.durations_vector = tuple(self.durations_vector)
@@ -148,26 +142,19 @@ class StockStatistics:
 
         self.average_holding_period = average(self.durations_vector)
         self.stdev_holding_period = std(self.durations_vector)
+        self.efficiency = self.total_holding_period / self.period
 
 
     def to_dataframe(self):
+        """ This class creates pandas DataFrame object containing the statistics for display"""
         pass
         # TODO
 
 
-
 class SystemStatistics(StockStatistics):
-    def __init__(self, system_name, period, durations_vector, yields_points_vector, yields_percentages_vector,
-                 start_date, end_date):
-
-        StockStatistics.__init__(self, system_name, period, durations_vector, yields_points_vector,
-                                 yields_percentages_vector)
-
-        self.start_date = start_date
-        self.end_date = end_date
-        # TODO add the additional fields
-
-
-    def calculate_statistics(self):
-        # TODO override and calculate the additional fields
-        pass
+    """
+    This is the system statistics class, it extends the stock statistics class.
+    [At the moment there is no different between them, so it is just the infrastructure]
+    """
+    pass
+    # TODO add fields: average open trades at single point over time, max open trades on single point of time (may required to add dates per trade)
