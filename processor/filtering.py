6ce365919068a5dc112185b07dabf8d959d79e3d
Tzion Sasson <tzion12345@gmail.com>
2016-07-05 01:34:05 +0300

    charts prints wip, stash shit, form file is broken

--- processor/filtering.py
+++ processor/filtering.py
@@ -73,6 +73,7 @@ def _mark_trigger_lines(trade_system, symbol, stock_data):
             previous_price = stock_data.get_value(row - 1, COLUMNS.close) if row > 0 else None
             if is_stop_loss_reached(current_price, entering_price, trade_system.get_stop_loss(), trade_system.get_direction()) or \
                     is_stop_loss_reached(current_price, previous_price, trade_system.get_moving_stop_loss(), trade_system.get_direction()):
+
                 #log.info("Trade reached stop-loss point, setting close trigger. symbol: {}, date: {}".format(symbol, date))
                 stock_data.set_value(row+1, COLUMNS.close_trigger, True)
                 entering_price = None
